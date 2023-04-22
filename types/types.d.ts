c5efef0af01f23ea0de7793c8c50f368bfa1b496
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2023-04-22 14:36:16 -0400

    publish job dependant on build job

--- types/types.d.ts
+++ types/types.d.ts
@@ -5,11 +5,11 @@ type ChatgptRole = 'user' | 'assistant' | 'system';
 type ChatgptMessage = {
   role: ChatgptRole;
   content: string;
-} ;
+};
 type ChatgptMessages = ChatgptMessage[];
 
 declare module 'openai' {
   const OpenAIApi: any;
   const Configuration: any;
   export { OpenAIApi, Configuration };
-};
+}
